59113b338c2180d17503b9c3cfa019b532d099e1
MexoSa <27315902@mail.ru>
2022-08-31 23:24:28 +0300

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# GitHub Pages
+
+You can see results of this project in [GitHub Pages](https://mexosa.github.io/FeShop/).
--- scripts/login.js
+++ scripts/login.js
@@ -6,74 +6,81 @@ const enterContainer = document.querySelector('.header__user');
 let currentUser = {};
 
 const usersList = [
-   {
-      login: 'Alex',
-      password: '1234',
-      name: 'Александр',
-      surname: 'Герасимов',
-      avatar: "./img/user-avatar.jpg"
-   }
+  {
+    login: 'Alex',
+    password: '1234',
+    name: 'Александр',
+    surname: 'Герасимов',
+    avatar: "./img/user-avatar.jpg"
+  }
 ]
 
+function stopRefresh(e) {
+  e.preventDefault()
+}
+
+const loginForm = document.querySelector('.user__container')
+loginForm.addEventListener('submit', stopRefresh)
+
 function toggleClassUserContainer() {
-   userContainer.classList.toggle('active');
+  userContainer.classList.toggle('active');
 }
 
 function checkedUser() {
-   currentUser = usersList.filter(({ login }) => login === loginInput.value).filter(({ password }) => password === passwordInput.value)
-   if (currentUser.length >= 1) {
-      [currentUser] = currentUser
-      toggleClassUserContainer()
-      updateStatusLogin()
-      setTimeout(() => {
-         changeUserContainer(currentUser)
-      }, 400);
-   } else {
-      showWrongDataMessage();
-   }
-   loginInput.value = ''
-   passwordInput.value = ''
+  currentUser = usersList.filter(({ login }) => login === loginInput.value).filter(({ password }) => password === passwordInput.value)
+  if (currentUser.length >= 1) {
+    [currentUser] = currentUser
+    toggleClassUserContainer()
+    updateStatusLogin()
+    setTimeout(() => {
+      changeUserContainer(currentUser)
+    }, 400);
+  } else {
+    showWrongDataMessage();
+  }
+  loginInput.value = ''
+  passwordInput.value = ''
 }
 
 function showWrongDataMessage() {
-   userContainer.classList.add('error')
-   setTimeout(() => {
-      userContainer.classList.remove('error')
-   }, 3000)
+  userContainer.classList.add('error')
+  setTimeout(() => {
+    userContainer.classList.remove('error')
+  }, 3000)
 }
 
 function changeUserContainer({ name, surname, avatar }) {
-   enterContainer.innerHTML = `
+  enterContainer.innerHTML = `
       <img class='user__avatar' src='${avatar}' alt=''>
       <div> ${name} ${surname}</div>`
 }
 
 function updateStatusLogin() {
-   localStorage.setItem('user', JSON.stringify(currentUser))
+  localStorage.setItem('user', JSON.stringify(currentUser))
 }
 
 function isUserLogin() {
-   currentUser = JSON.parse(localStorage.getItem('user')) ?? {};
-   if (Object.keys(currentUser).length > 0) {
-      changeUserContainer(currentUser);
-   } else {
-      const loginBtn = document.createElement('button');
-      loginBtn.classList.add('user__btn');
-      loginBtn.innerHTML = 'Войти'
-      enterContainer.prepend(loginBtn);
-      loginBtn.addEventListener('click', function showUserLoginBox() {
-         toggleClassUserContainer()
-         loginInput.focus()
-         loginBtn.removeEventListener('click', showUserLoginBox);
-         setTimeout(() => {
-            loginBtn.addEventListener('click', showUserLoginBox);
-         }, 400)
-      })
-   }
+  currentUser = JSON.parse(localStorage.getItem('user')) ?? {};
+  if (Object.keys(currentUser).length > 0) {
+    changeUserContainer(currentUser);
+  } else {
+    const loginBtn = document.createElement('button');
+    loginBtn.classList.add('user__btn');
+    loginBtn.innerHTML = 'Войти'
+    enterContainer.prepend(loginBtn);
+    loginBtn.addEventListener('click', function showUserLoginBox() {
+      toggleClassUserContainer()
+      loginInput.focus()
+      loginBtn.removeEventListener('click', showUserLoginBox);
+      setTimeout(() => {
+        loginBtn.addEventListener('click', showUserLoginBox);
+      }, 400)
+    })
+  }
 }
 
 entranceBtn.addEventListener('click', () => {
-   checkedUser();
+  checkedUser();
 })
 
 isUserLogin();
